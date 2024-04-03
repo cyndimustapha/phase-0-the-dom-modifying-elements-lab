eac5bb1f2551135231228aa760196e26bc2437ef
Cyndi Marren Mustapha <marrencyndi101@gmail.com>
2024-04-03 15:15:14 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code here!
+const main = document.querySelector("#main");
+
+main.remove();
+
+const newHeader = document.createElement("H1");
+
+newHeader.id = 'victory';
+
+newHeader.innerHTML ="Cyndi is the champion";
+
